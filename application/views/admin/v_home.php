1d524a2fa1808a03142b560b5d810de0084930d2
ichsanarsyi <49037429+ichsanarsyi@users.noreply.github.com>
2021-05-06 15:02:10 +0700

    update database

--- application/views/admin/v_home.php
+++ application/views/admin/v_home.php
@@ -1,7 +1,7 @@
 <div class="col-lg-12">
     <div class="panel panel-primary">
         <div class="panel-heading">
-			Selamat datang di halaman Admin
+			Selamat datang <?= $this->session->userdata('nama_user') ?>
         </div>
         <div class="panel-body">
 			<h4>Jumlah Guru: <?= $this->db->from("tbl_guru")->count_all_results(); ?></h4>	
--- web-sekolah.sql
+++ web-sekolah.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 03, 2021 at 12:30 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 7.4.15
+-- Generation Time: May 06, 2021 at 09:57 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,51 +24,12 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_berita`
---
-
-CREATE TABLE `tbl_berita` (
-  `id_berita` int(11) NOT NULL,
-  `judul_berita` varchar(255) DEFAULT NULL,
-  `slug_berita` varchar(255) NOT NULL,
-  `isi_berita` text DEFAULT NULL,
-  `gambar_berita` varchar(30) DEFAULT NULL,
-  `tgl_berita` timestamp NOT NULL DEFAULT current_timestamp(),
-  `id_user` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `tbl_berita`
---
-
-INSERT INTO `tbl_berita` (`id_berita`, `judul_berita`, `slug_berita`, `isi_berita`, `gambar_berita`, `tgl_berita`, `id_user`) VALUES
-(2, 'Pendidikan Kab. Semarang Terus Dibenahi', 'Pendidikan-Kab-Semarang-Terus-Dibenahi', '<h1><big><strong>Pendidikan</strong></big></h1>\r\n\r\n<p>Pendidikan adalah pembelajaran pengetahuan, keterampilan, dan kebiasaan sekelompok orang yang diturunkan dari satu generasi ke generasi berikutnya melalui pengajaran, pelatihan, atau penelitian. Pendidikan sering terjadi di bawah bimbingan orang lain, tetapi juga memungkinkan secara otodidak.</p>\r\n', '178622-love-girl.jpg', '2021-02-21 17:00:00', 1),
-(3, 'Janji-Janji', 'Janji-Janji', '<p>Pasangan adalah orang penting lainnya dalam pernikahan, ikatan sipil, atau pernikahan hukum adat. Istilahnya netral gender, sedangkan pasangan laki-laki adalah suami dan pasangan perempuan adalah istri.</p>\r\n', '177237-love-couple.jpg', '2021-02-21 17:00:00', 1),
-(4, 'Hoax', 'Hoax', '<p>Berita bohong atau berita palsu atau hoaks adalah informasi yang sesungguhnya tidak benar, tetapi dibuat seolah-olah benar adanya. Hal ini tidak sama dengan rumor, ilmu semu, maupun April Mop. Tujuan dari berita bohong adalah membuat masyarakat merasa tidak aman, tidak nyaman, dan kebingungan.</p>\r\n', 'love_story_(1).jpg', '2021-02-21 17:00:00', 1),
-(5, 'Buana Buaya', 'Buana-Buaya', '<p>Buana dan Buaya tak akan pernah satu.</p>\r\n', '178620-love-cute-couple.jpg', '2021-02-21 17:00:00', 1),
-(6, 'Suara Sekolah Radio Kita', 'Suara-Sekolah-Radio-Kita', '<blockquote>\r\n<h2><s><em><strong>Yuk Gabung Ke Tim Penyiar Radio Suara Sekolah</strong></em></s></h2>\r\n</blockquote>\r\n', '187979-love-cute-couple.jpg', '2021-02-21 17:00:00', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_file`
---
-
-CREATE TABLE `tbl_file` (
-  `id_file` int(11) NOT NULL,
-  `nama_file` varchar(255) DEFAULT NULL,
-  `file` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `tbl_guru`
 --
 
 CREATE TABLE `tbl_guru` (
   `id_guru` int(11) NOT NULL,
-  `nip` varchar(20) DEFAULT NULL,
+  `no_id` varchar(20) DEFAULT NULL,
   `nama_guru` varchar(25) DEFAULT NULL,
   `tempat_lahir` varchar(15) DEFAULT NULL,
   `tgl_lahir` date DEFAULT NULL,
@@ -81,12 +42,12 @@ CREATE TABLE `tbl_guru` (
 -- Dumping data for table `tbl_guru`
 --
 
-INSERT INTO `tbl_guru` (`id_guru`, `nip`, `nama_guru`, `tempat_lahir`, `tgl_lahir`, `id_mapel`, `pendidikan`, `foto_guru`) VALUES
-(3, '3333333', 'Putra', 'Semarang', '2021-02-21', 2, 'S2', 'gmb_kompas.gif'),
-(4, '4444444', 'Sassan', 'Semarang', '2021-02-21', 3, 'S1', 'Quote.png'),
-(5, '123', 'Sirajuddin', 'Bekasi', '1990-01-12', 1, 'S2', 'yosi_merah_4x6.jpg'),
-(6, '1234', 'Kirishima Touka', 'Tokyo', '2021-02-06', 5, 'S1', 'Pamflet_AMDIC.png'),
-(7, '2345', 'Unyil', 'Bekasi', '2021-02-02', 3, 'S2', 'Fantasia_Painting(2).jpg');
+INSERT INTO `tbl_guru` (`id_guru`, `no_id`, `nama_guru`, `tempat_lahir`, `tgl_lahir`, `id_mapel`, `pendidikan`, `foto_guru`) VALUES
+(3, '10001', 'Putra', 'Semarang', '2021-02-21', 2, 'S2', 'gmb_kompas.gif'),
+(4, '10002', 'Sassan', 'Semarang', '2021-02-21', 3, 'S1', 'Quote.png'),
+(5, '10003', 'Sirajuddin', 'Bekasi', '1990-01-12', 1, 'S2', 'yosi_merah_4x6.jpg'),
+(6, '10004', 'Kirishima Touka', 'Tokyo', '2021-02-06', 5, 'S1', 'Pamflet_AMDIC.png'),
+(7, '10005', 'Unyil', 'Bekasi', '2021-02-02', 3, 'S2', 'Fantasia_Painting(2).jpg');
 
 -- --------------------------------------------------------
 
@@ -116,33 +77,12 @@ INSERT INTO `tbl_mapel` (`id_mapel`, `nama_mapel`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_pengumuman`
---
-
-CREATE TABLE `tbl_pengumuman` (
-  `id_pengumuman` int(11) NOT NULL,
-  `judul_pengumuman` varchar(255) DEFAULT NULL,
-  `isi_pengumuman` text DEFAULT NULL,
-  `tgl` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `tbl_pengumuman`
---
-
-INSERT INTO `tbl_pengumuman` (`id_pengumuman`, `judul_pengumuman`, `isi_pengumuman`, `tgl`) VALUES
-(2, 'UTS', 'ini isi pengumuman lhoo', '2021-02-08'),
-(3, 'Ulangan', 'Ulangan Harian batalll', '2021-02-22');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `tbl_siswa`
 --
 
 CREATE TABLE `tbl_siswa` (
   `id_siswa` int(11) NOT NULL,
-  `nis` varchar(15) DEFAULT NULL,
+  `no_id` varchar(20) DEFAULT NULL,
   `nama_siswa` varchar(25) DEFAULT NULL,
   `tempat_lahir` varchar(15) DEFAULT NULL,
   `tgl_lahir` date DEFAULT NULL,
@@ -154,11 +94,11 @@ CREATE TABLE `tbl_siswa` (
 -- Dumping data for table `tbl_siswa`
 --
 
-INSERT INTO `tbl_siswa` (`id_siswa`, `nis`, `nama_siswa`, `tempat_lahir`, `tgl_lahir`, `kelas`, `foto_siswa`) VALUES
-(2, '11112', 'Exaan', 'Semarang', '2021-02-02', 'II', 'peta-banten.jpg'),
-(3, '11113', 'San', 'Bogor', '2021-02-10', 'III', 'Firefox-logo_svg.png'),
-(4, '11114', 'Gru', 'Ciamis', '2021-02-07', 'I', 'FA-chowking-counter-standee-30-x-45-cm-SP-just-1.jpg'),
-(5, '151515', 'Udin Penyok', 'NTT', '2000-06-15', 'XI', 'mmexport1385272967723.jpg');
+INSERT INTO `tbl_siswa` (`id_siswa`, `no_id`, `nama_siswa`, `tempat_lahir`, `tgl_lahir`, `kelas`, `foto_siswa`) VALUES
+(2, '90001', 'Exaan', 'Semarang', '2021-02-02', 'X', 'peta-banten.jpg'),
+(3, '90002', 'San', 'Bogor', '2021-02-10', 'XII', 'Firefox-logo_svg.png'),
+(4, '90003', 'Gru', 'Ciamis', '2021-02-07', 'XII', 'FA-chowking-counter-standee-30-x-45-cm-SP-just-1.jpg'),
+(5, '90004', 'Udin Penyok', 'NTT', '2000-06-15', 'XI', 'mmexport1385272967723.jpg');
 
 -- --------------------------------------------------------
 
@@ -168,6 +108,7 @@ INSERT INTO `tbl_siswa` (`id_siswa`, `nis`, `nama_siswa`, `tempat_lahir`, `tgl_l
 
 CREATE TABLE `tbl_user` (
   `id_user` int(3) NOT NULL,
+  `no_id` varchar(20) DEFAULT NULL,
   `nama_user` varchar(25) DEFAULT NULL,
   `username` varchar(25) DEFAULT NULL,
   `password` varchar(25) DEFAULT NULL,
@@ -178,25 +119,21 @@ CREATE TABLE `tbl_user` (
 -- Dumping data for table `tbl_user`
 --
 
-INSERT INTO `tbl_user` (`id_user`, `nama_user`, `username`, `password`, `level`) VALUES
-(1, 'Ichsan Arsyi Putra', 'admin', 'admin', 1),
-(2, 'User', 'user', 'user', 2);
+INSERT INTO `tbl_user` (`id_user`, `no_id`, `nama_user`, `username`, `password`, `level`) VALUES
+(1, '00000', 'Admin', 'admin', 'admin', 1),
+(2, '10001', 'Putra', 'putra', 'putra', 2),
+(3, '10002', 'Sassan', 'sassan', 'sassan', 2),
+(4, '10003', 'Sirajuddin', 'sirajuddin', 'sirajuddin', 2),
+(5, '10004', 'Kirishima Touka', 'kirishima', 'kirishima', 2),
+(6, '10005', 'Unyil', 'unyil', 'unyil', 2),
+(7, '90001', 'Exaan', 'exaan', 'exaan', 2),
+(8, '90002', 'San', 'san', 'san', 2),
+(9, '90003', 'Gru', 'gru', 'gru', 2),
+(10, '90004', 'Udin Penyok', 'udin', 'udin', 2);
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `tbl_berita`
---
-ALTER TABLE `tbl_berita`
-  ADD PRIMARY KEY (`id_berita`);
-
---
--- Indexes for table `tbl_file`
---
-ALTER TABLE `tbl_file`
-  ADD PRIMARY KEY (`id_file`);
 
 --
 -- Indexes for table `tbl_guru`
@@ -209,12 +146,6 @@ ALTER TABLE `tbl_guru`
 --
 ALTER TABLE `tbl_mapel`
   ADD PRIMARY KEY (`id_mapel`);
-
---
--- Indexes for table `tbl_pengumuman`
---
-ALTER TABLE `tbl_pengumuman`
-  ADD PRIMARY KEY (`id_pengumuman`);
 
 --
 -- Indexes for table `tbl_siswa`
@@ -233,18 +164,6 @@ ALTER TABLE `tbl_user`
 --
 
 --
--- AUTO_INCREMENT for table `tbl_berita`
---
-ALTER TABLE `tbl_berita`
-  MODIFY `id_berita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `tbl_file`
---
-ALTER TABLE `tbl_file`
-  MODIFY `id_file` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `tbl_guru`
 --
 ALTER TABLE `tbl_guru`
@@ -257,12 +176,6 @@ ALTER TABLE `tbl_mapel`
   MODIFY `id_mapel` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
--- AUTO_INCREMENT for table `tbl_pengumuman`
---
-ALTER TABLE `tbl_pengumuman`
-  MODIFY `id_pengumuman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
 -- AUTO_INCREMENT for table `tbl_siswa`
 --
 ALTER TABLE `tbl_siswa`
@@ -272,7 +185,7 @@ ALTER TABLE `tbl_siswa`
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `id_user` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_user` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
